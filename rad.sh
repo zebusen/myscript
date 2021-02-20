fd08b869fa5a027624d4f7a031b98844b3f21975
zebusen <79355186+zebusen@users.noreply.github.com>
2021-02-20 08:48:08 -0800

    Create rad.sh
    Add script for rad kernel

---
+++ rad.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+echo "Cloning dependencies"
+git clone --depth=1 --quiet https://github.com/kdrag0n/proton-clang clang
+git clone --depth=1 https://github.com/sohamxda7/AnyKernel3 AnyKernel
+echo "Done"
+IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
+TANGGAL=$(date +"%F-%S")
+START=$(date +"%s")
+KERNEL_DIR=$(pwd)
+PATH="${PWD}/clang/bin:$PATH"
+export KBUILD_COMPILER_STRING="$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
+export ARCH=arm64
+export KBUILD_BUILD_HOST=circleci
+export KBUILD_BUILD_USER="mikofan"
+function sendinfo() {
+    curl -s -X POST "https://api.telegram.org/bot1628360095:AAF947lAXmKVaw9jRpx-CURb_wK2FZKl9z8/sendMessage" \
+        -d chat_id="-1001214166550" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=html" \
+        -d text="cook"
+}
+# Push kernel to channel
+function push() {
+    cd AnyKernel
+    ZIP=$(echo *.zip)
+    curl -F document=@$ZIP "https://api.telegram.org/bot1628360095:AAF947lAXmKVaw9jRpx-CURb_wK2FZKl9z8/sendDocument" \
+        -F chat_id="-1001214166550" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>WHYRED</b> | using Proton Clang (I'll update this everytime when I change compiler)"
+}
+# Fin Error
+function finerr() {
+    curl -s -X POST "https://api.telegram.org/bot1628360095:AAF947lAXmKVaw9jRpx-CURb_wK2FZKl9z8/sendMessage" \
+        -d chat_id="-1001214166550" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=markdown" \
+        -d text="Build throw an error(s)"
+    exit 1
+}
+# Compile plox
+function compile() {
+    cd /root/project/android_kernel_xiaomi_whyred
+    make O=out ARCH=arm64 whyred_defconfig
+    make -j$(nproc --all) O=out \
+                          ARCH=arm64 \
+			  CC=clang \
+			  CROSS_COMPILE=aarch64-linux-gnu- \
+			  CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+    cp out/arch/arm64/boot/Image.gz-dtb /root/project/AnyKernel
+}
+# Zipping
+function zipping() {
+    cd /root/project/AnyKernel || exit 1
+    zip -r9 personal-hmp-rad.zip *
+    cd ..
+}
+sendinfo
+compile
+zipping
+END=$(date +"%s")
+DIFF=$(($END - $START))
+push
