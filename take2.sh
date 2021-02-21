0b0b6c5cbe608cef075323c81d94b04f14f0cc72
zebusen <79355186+zebusen@users.noreply.github.com>
2021-02-21 16:21:36 +0800

    Create take2.sh

---
+++ take2.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/env bash
+echo "Cloning dependencies"
+
+ clone() {
+	echo " "
+		msg "|| Cloning GCC 9.3.0 baremetal ||"
+		git clone --depth=1 https://github.com/mvaisakh/gcc-arm64.git gcc64
+		git clone --depth=1 https://github.com/mvaisakh/gcc-arm.git gcc32
+		GCC64_DIR=$KERNEL_DIR/gcc64
+		GCC32_DIR=$KERNEL_DIR/gcc32
+
+	msg "|| Cloning Anykernel ||"
+	git clone --depth 1 https://github.com/osm0sis/AnyKernel3.git
+}
+
+exports() {
+	export KBUILD_BUILD_USER="reina"
+	export KBUILD_COMPILER_STRING="GCC 10.2 LTO"
+	export ARCH=arm64
+	export SUBARCH=arm64
+
+	KBUILD_COMPILER_STRING=$("$GCC64_DIR"/bin/aarch64-elf-gcc --version | head -n 1)
+	PATH=$GCC64_DIR/bin/:$GCC32_DIR/bin/:/usr/bin:$PATH
+
+	export PATH KBUILD_COMPILER_STRING
+	export BOT_MSG_URL="https://api.telegram.org/bot1628360095:AAF947lAXmKVaw9jRpx-CURb_wK2FZKl9z8/sendMessage"
+	export BOT_BUILD_URL="https://api.telegram.org/bot1628360095:AAF947lAXmKVaw9jRpx-CURb_wK2FZKl9z8/sendDocument"
+	PROCS=$(nproc --all)
+	export PROCS
+}
+tg_post_msg() {
+	curl -s -X POST "$BOT_MSG_URL" -d chat_id="-1001214166550" \
+	-d "disable_web_page_preview=true" \
+	-d "parse_mode=html" \
+	-d text="$1"
+
+}
+
+tg_post_build() {
+	#Post MD5Checksum alongwith for easeness
+	MD5CHECK=$(md5sum "$1" | cut -d' ' -f1)
+
+	#Show the Checksum alongwith caption
+	curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
+	-F chat_id="-1001214166550"  \
+	-F "disable_web_page_preview=true" \
+	-F "parse_mode=html" \
+	-F caption="$3 | <code>Build Number : </code><b>$DRONE_BUILD_NUMBER</b>"  
+}
+build_kernel() {
+	msg "|| Started Compilation ||"
+	export CROSS_COMPILE_ARM32=$GCC32_DIR/bin/arm-eabi-
+  cd /root/project/android_kernel_xiaomi_whyred
+	make -j"$PROCS" O=out whyred_defconfig CROSS_COMPILE=aarch64-elf-
+
+		BUILD_END=$(date +"%s")
+		DIFF=$((BUILD_END - BUILD_START))
+
+		if [ -f "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb ] 
+	    then
+	    	msg "|| Kernel successfully compiled ||"
+	    	if [ $BUILD_DTBO = 1 ]
+			then
+				msg "|| Building DTBO ||"
+				tg_post_msg "<code>Building DTBO..</code>" "$CHATID"
+				python2 "$KERNEL_DIR/scripts/ufdt/libufdt/utils/src/mkdtboimg.py" \
+					create "$KERNEL_DIR/out/arch/arm64/boot/dtbo.img" --page_size=4096 "$KERNEL_DIR/out/arch/arm64/boot/dts/qcom/sm6150-idp-overlay.dtbo"
+			fi
+				gen_zip
+		else
+			if [ "$PTTG" = 1 ]
+ 			then
+				tg_post_msg "<b>❌ Build failed to compile after $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds</b>" "$CHATID"
+			fi
+		fi
+	
+}
+gen_zip() {
+    cd /root/project/AnyKernel || exit 1
+    zip -r9 personal-hmp-rad.zip *
+    cd ..
+}
+clone
+exports
+build_kernel
