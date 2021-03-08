0d83366ee7251ca34696d82fea45e7d5342e70cd
zebusen <79355186+zebusen@users.noreply.github.com>
2021-03-08 17:06:51 +0800

    Update radeas.sh

--- radeas.sh
+++ radeas.sh
@@ -10,8 +10,8 @@ KERNEL_DIR=$(pwd)
 PATH="${PWD}/clang/bin:$PATH"
 export KBUILD_COMPILER_STRING="$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
 export ARCH=arm64
-export KBUILD_BUILD_HOST=circleci
-export KBUILD_BUILD_USER="miko35"
+export KBUILD_BUILD_HOST=miyauchi
+export KBUILD_BUILD_USER="renge"
 function sendinfo() {
     curl -s -X POST "https://api.telegram.org/bot1628360095:AAF947lAXmKVaw9jRpx-CURb_wK2FZKl9z8/sendMessage" \
         -d chat_id="-1001214166550" \
@@ -49,13 +49,25 @@ function compile() {
 			  CROSS_COMPILE_ARM32=arm-linux-gnueabi-
     cp out/arch/arm64/boot/Image.gz-dtb /root/project/AnyKernel
 }
+function fixcharger() {
+echo "Add commit for fixing charging"
+cd /root/project/android_kernel_xiaomi_whyred
+git config uploadpack.allowReachableSHA1InWant true
+git remote add zebu2 https://github.com/SreekanthPalakurthi/kranul
+git fetch --shallow-since=2020-06-31 zebu2
+git cherry-pick f14650b1984e23a1304eaee8aeae629414c92801
+git cherry-pick c491c59c4a762a03b41078707a0f08f0c8e2431f
+git cherry-pick b2c890885a0cee05f12e9d8910168c8099248023
+git cherry-pick fd00fab3ffcee93bea2fda00e21ddf8eb24630c1
+}
 # Zipping
 function zipping() {
     cd /root/project/AnyKernel || exit 1
-    zip -r9 personal-eas-rad.zip *
+    zip -r9 personal-eas-rad$(date).zip *
     cd ..
 }
 sendinfo
+fixcharger
 compile
 zipping
 END=$(date +"%s")
